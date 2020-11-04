94e29c3d8a9f5e3e6ae6e956cbb221a2842fa286
erhan güven <erhangvn7@gmail.com>
2020-11-04 19:48:37 +0300

    Player movement and physics -1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviourPun
+{
+    public float speed;
+
+    void Awake()
+    {
+        if (!photonView.IsMine)
+        {
+            this.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime*speed*(Vector3.right * Input.GetAxis("Horizontal") + Vector3.forward * Input.GetAxis("Vertical"));
+    }
+}
