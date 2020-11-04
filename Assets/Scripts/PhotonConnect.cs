a138e869a42619fb658d1c775f4183515d5daba3
Calquter <73954471+Calquter@users.noreply.github.com>
2020-11-04 19:22:32 +0300

    dene

--- Assets/Scripts/PhotonConnect.cs
+++ Assets/Scripts/PhotonConnect.cs
@@ -9,7 +9,7 @@ public class PhotonConnect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("b");
     }
 
     // Update is called once per frame
